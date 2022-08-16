f94115aab73e7427509cb4ebd33585c25e0ffab6
Anthony Catullo <anthonycatullo@Anthonys-MacBook-Pro.local>
2022-08-16 13:55:28 -0500

    deleted navbar hotfix

--- fe-youdemy/pages/_app.js
+++ fe-youdemy/pages/_app.js
@@ -1,11 +1,9 @@
 import "../styles/globals.css";
-import Navbar from "../components/navbar/Navbar.js";
 import Footer from "../components/footer/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
       <Footer />
     </>
