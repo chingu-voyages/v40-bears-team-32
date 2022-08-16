0a0e598d79a4f12fb04dec8fe33840a1d6345ffc
Anthony Catullo <anthonycatullo@Anthonys-MacBook-Pro.local>
2022-08-16 12:37:34 -0500

    passed content for TextBlockStretch component from about page to child

--- fe-youdemy/pages/about.js
+++ fe-youdemy/pages/about.js
@@ -2,6 +2,9 @@ import React from "react";
 import TextBlockStretch from "../components/TextBlockStretch.js";
 
 const About = () => {
+  const content = {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  };
   return (
     <div className="center">
       <div className="about-page-container">
@@ -16,7 +19,7 @@ const About = () => {
             </p>
           </div>
         </div>
-        <TextBlockStretch />
+        <TextBlockStretch content={content} />
       </div>
     </div>
   );
