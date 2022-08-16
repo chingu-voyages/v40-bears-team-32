b6b01ff2e2ebe8010385f4a735dd5bfcd9cfec3a
Anthony Catullo <anthonycatullo@Anthonys-MacBook-Pro.local>
2022-08-16 12:31:12 -0500

    added about page design

--- fe-youdemy/pages/about.js
+++ fe-youdemy/pages/about.js
@@ -1,11 +1,22 @@
 import React from "react";
+import TextBlockStretch from "../components/TextBlockStretch.js";
 
 const About = () => {
   return (
     <div className="center">
-      <div className="about-page">
-        <h1>What is YouDemy?</h1>
-        <img src="undraw_online_learning.svg" width={480} height={400} />
+      <div className="about-page-container">
+        <div className="about-page">
+          <h1>What is YouDemy?</h1>
+          <img src="undraw_online_learning.svg" width={480} height={400} />
+          <div className="about-text-group">
+            <p>These days you can learn everything online.</p>
+            <br></br>
+            <p>
+              YouDemy is the place for creators and students to get acquainted.
+            </p>
+          </div>
+        </div>
+        <TextBlockStretch />
       </div>
     </div>
   );
