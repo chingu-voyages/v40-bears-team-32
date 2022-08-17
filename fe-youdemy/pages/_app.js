f32348bd09317ac1ba2de87ceb0a494da13b87e2
Mateo Cruz <mtaycruz@gmail.com>
2022-08-17 19:47:56 -0400

    Fe hero 002 (#39)
    * inital build of header component
    
    * fix
    
    * fixed conflicts

--- fe-youdemy/pages/_app.js
+++ fe-youdemy/pages/_app.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+=======
 import "../styles/globals.css";
 import DrawerAppBar from "../components/navbar/DrawerAppBar.js";
 import Footer from "../components/footer/Footer";
@@ -10,6 +16,7 @@ function MyApp({ Component, pageProps }) {
       <Footer />
     </>
   );
+>>>>>>> 09a0cac02eca0052d9c0e56355ab9d9ba63aea86
 }
 
-export default MyApp;
+export default MyApp
