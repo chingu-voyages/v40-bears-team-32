bd0d671027b9230f5017630632a1a14228b8ca5c
riboney <14286113+riboney@users.noreply.github.com>
2022-08-16 23:48:55 -0400

    BE/replace console.log with logger

--- be-youdemy/src/index.js
+++ be-youdemy/src/index.js
@@ -1,19 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import connectDB from "./config/db.js";
+import { logger } from "./config";
 
 dotenv.config();
 const app = express();
 connectDB();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
