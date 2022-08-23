a6d5e12c7f83c0d338dbf7d712aeb86097fabfab
Joshua Nho <joshnho@gmail.com>
2022-08-23 01:02:30 -0400

    added comments to protectRoute file

--- be-youdemy/src/middleware/auth.js
+++ be-youdemy/src/middleware/auth.js
@@ -2,13 +2,16 @@ import jwt from 'jsonwebtoken';
 
 import User from '../models/userSchema';
 
-export const protectRoute = async (req, res, next) => {
+export const protectRoute = (req, res, next) => {
   const token = req.cookies.token;
 
   if (token) {
+    // Verify/decode token
     jwt.verify(token, process.env.SESSION_SECRET, async (err, decoded) => {
       if (err) next();
+      // If token is valid, search for user with corresponding ID from decoded token
       const user = await User.findById(decoded.id);
+      // Set custom user field into the request with current user data
       req.user = user;
     });
   }
