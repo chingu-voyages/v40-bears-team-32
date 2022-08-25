c7f42127a236dfdf987e0baa6318f2632b22042a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-08-24 22:33:41 -0400

    create video controller

--- be-youdemy/src/controllers/video/videosController.js
+++ be-youdemy/src/controllers/video/videosController.js
@@ -12,3 +12,31 @@ export const getAllVideos = async (req, res) => {
         res.status(500).send('Error' + error);
     }
 }
+
+// Create Video
+export const createVideo = async (req, res) => {
+    try { 
+        const { title, description, videoSource, comment } = req.body;
+    
+        const video = await Video.create({
+            title,
+            description,
+            videoSource,
+            comment,
+        });
+    
+        const newVideo = {
+            "title": video.title,
+            "description": video.description,
+            "videoSource": video.videoSource,
+            "comment": video.comment
+        }
+    
+        res.json(newVideo);
+    } 
+    catch (error) {
+        logger.info(error.message);
+        res.status(500).send('Server error');
+    }
+
+};
