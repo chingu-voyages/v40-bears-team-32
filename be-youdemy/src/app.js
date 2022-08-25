3baf4921bc205ac0cd7c5a490a5ffed245eeb5a1
Josh Nho <joshnho@gmail.com>
2022-08-25 19:23:26 -0400

    added protectRoute middleware (#45)
    * added protectRoute middleware
    
    * Update auth.js
    
    * added comments to protectRoute file
    
    * added comments to protectRoute file

--- be-youdemy/src/app.js
+++ be-youdemy/src/app.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import path from 'path';
 
 import authRoutes from './routes/auth/authRoutes.js';
 
 import morgan from './middleware/morgan.js';
+import { protectRoute } from './middleware/auth.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan);
+app.use(protectRoute());
 
 app.use('/auth', authRoutes);
 
