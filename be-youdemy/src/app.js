8f7b888a138787266fc93aebc263933b7bdae8cc
Joshua Nho <joshnho@gmail.com>
2022-08-28 05:58:54 -0400

    commented out prod code

--- be-youdemy/src/app.js
+++ be-youdemy/src/app.js
@@ -16,11 +16,11 @@ app.use(morgan);
 app.use('/auth', authRoutes);
 app.use('/videos', videoRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 export default app;
