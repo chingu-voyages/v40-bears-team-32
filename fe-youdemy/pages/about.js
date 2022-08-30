eb6ed57bc234c69daebd581492eb53d12836678f
Mateo <mtaycruz@gmail.com>
2022-08-30 12:28:57 -0400

    hero added

--- fe-youdemy/pages/about.js
+++ fe-youdemy/pages/about.js
@@ -45,7 +45,7 @@ const About = () => {
   return (
     <div className="center">
       <div className="about-page-container">
-        <Hero />
+        
         <div className="about-page">
           <h1>What is YouDemy?</h1>
           <img src="undraw_online_learning.svg" width={480} height={400} />
