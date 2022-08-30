5fdaf3b7471eaab10a33382062b633a3dc81060a
Anthony Catullo <anthonycatullo@Anthonys-MacBook-Pro.local>
2022-08-30 16:18:39 -0500

    modified styles of search bar and made skills field a string and searchable

--- fe-youdemy/pages/search.js
+++ fe-youdemy/pages/search.js
@@ -55,7 +55,7 @@ export default function Search() {
         "https://images.unsplash.com/photo-1628890923662-2cb23c2e0cfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
       name: "Miranda G",
       headline: "I've been teaching JavaScript for 10 years.",
-      skills: ["JavaScript", "ReactJS", "SQL", "Ruby on Rails"],
+      skills: "JavaScript, ReactJS, SQL, Ruby on Rails",
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
@@ -65,7 +65,7 @@ export default function Search() {
         "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
       name: "Anthony K",
       headline: "I've been teaching Java for 5 years.",
-      skills: ["Java", "Angular", "SQL"],
+      skills: "Java, Angular, SQL",
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
@@ -75,7 +75,7 @@ export default function Search() {
         "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
       name: "Steve J",
       headline: "I've been teaching SQL for 8 years.",
-      skills: ["Oracle", "PostgreSQL", "MySQL"],
+      skills: "Oracle, PostgreSQL, MySQL",
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
@@ -85,7 +85,7 @@ export default function Search() {
         "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
       name: "Jatinder R",
       headline: "I've been teaching Product Management for 5 years.",
-      skills: ["Jira", "Notion", "Scrum"],
+      skills: "Jira, Notion, Scrum",
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
@@ -95,7 +95,7 @@ export default function Search() {
         "https://images.unsplash.com/photo-1619895862022-09114b41f16f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
       name: "Satisha L",
       headline: "I've been teaching C++ for 15 years.",
-      skills: ["C++", "CyberSecurity", "Binary Exploitation"],
+      skills: "C++, CyberSecurity, Binary Exploitation",
       description:
         "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     },
@@ -106,11 +106,12 @@ export default function Search() {
 
   const onChange = (event) => {
     setInput(event.target.value);
-    const filteredResults = [...results].filter((result) => {
-      if (event.target.result === "") {
+    const filteredResults = results.filter((result) => {
+      if (!input) {
         return result;
       } else if (
-        result.headline.toLowerCase().includes(event.target.value.toLowerCase())
+        result.headline.toLowerCase().includes(input.toLowerCase()) ||
+        result.skills.toLowerCase().includes(input.toLowerCase())
       ) {
         return result;
       }
@@ -135,6 +136,7 @@ export default function Search() {
             onChange={onChange}
             value={input}
             autoFocus
+            style={{ width: "100%" }}
           />
         </Search>
       </div>
